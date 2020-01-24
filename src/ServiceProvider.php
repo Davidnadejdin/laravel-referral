26cd4f55f717ab60e4a91aceb197a58f52208172
davidnadejdin <davidnadejdin@gmail.com>
2020-01-24 10:34:12 +0500

    Base packages and provider

---
+++ src/ServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Davidnadejdin\LaravelReferral;
+
+use Illuminate\Support\ServiceProvider as BaseProvider;
+
+class ServiceProvider extends BaseProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
